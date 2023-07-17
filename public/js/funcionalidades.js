9d304486318f625095c6b8b61e67219837602ac7
Braian123548 <braiandelprado1@gmail.com>
2023-07-17 11:58:34 -0300

    Agregando posicionamiento con flex-box

--- public/js/funcionalidades.js
+++ public/js/funcionalidades.js
@@ -2,7 +2,6 @@ let navAbierto = true;
 
 function ocultarNav() {
   let nav = document.getElementById("home-header-nav")
-
   if (navAbierto) {
     nav.style.display = "none";
     navAbierto = false;
@@ -11,3 +10,26 @@ function ocultarNav() {
     navAbierto = true;
   }
 }
+
+
+
+const contenedoresTarjeta = document.querySelectorAll('.contenedor-tarjeta');
+const h3Elements = document.querySelectorAll('.home-main h3');
+
+function actualizarPosicionH3() {
+  contenedoresTarjeta.forEach((contenedor, index) => {
+    const primerHijo = contenedor.firstElementChild;
+    const h3Element = h3Elements[index];
+
+    const posicionPrimerHijo = primerHijo.getBoundingClientRect();
+    const margenIzquierdo = posicionPrimerHijo.left - contenedor.getBoundingClientRect().left;
+
+    h3Element.style.marginLeft = `${margenIzquierdo}px`;
+  });
+}
+
+// Ejecutar la función inicialmente y cada vez que se redimensione la ventana
+window.addEventListener('DOMContentLoaded', actualizarPosicionH3);
+window.addEventListener('resize', actualizarPosicionH3);
+
+
