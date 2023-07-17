9bfc76b3c50880182d64bbe40d82079821de5b76
Braian123548 <braiandelprado1@gmail.com>
2023-07-17 14:37:12 -0300

    Agregando login y register

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.listen(3030,()=>{
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "views", "home.html"))
 );
+
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
+
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
